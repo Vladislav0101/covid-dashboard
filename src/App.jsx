ba4e2b5637c123d8d72d8ad526684a820002d51a
DaryaYa <lyalyaka@mail.ru>
2020-12-13 03:31:02 +0300

    feat: header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
 import React from "react";
+import { Header } from './Components/Header'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
